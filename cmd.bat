197254d7d2ab21f22a3e6c6e2d2c94d761348bdc
andrewloski <38717839+andrewloski@users.noreply.github.com>
2018-04-26 15:16:02 +0800

    Create cmd.bat

---
+++ cmd.bat
@@ -0,0 +1,28 @@
+rem Windows command prompt
+rem cmd提权
+doskey su=runas /user:User cmd
+
+rem U盘分区
+diskpart
+list disk
+sel disk 1
+clean
+cre par pri size=8192
+active
+format fs=ntfs quick
+
+rem 磁盘分区
+List Disk
+Select Disk 0
+Clean
+Create Partition Primary Size=512000
+Active
+Format fs=ntfs Quick
+Create Partition Extended
+Create Partition Logical Size=512000
+Format fs=ntfs Quick
+Create Partition Logical Size=512000
+Format fs=ntfs Quick
+Create Partition Logical
+Format fs=ntfs Quick
+Exit
